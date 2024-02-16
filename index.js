2f5bb68833a5da0ec87e94fa958d81a7974bcf70
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-02-16 20:13:06 +0530

    done with get endpoint

--- index.js
+++ index.js
@@ -1,46 +1,53 @@
 const express = require("express");
 const { createTodo, updateTodo } = require("./type");
+const { todo } = require("./db");
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 const port = 3000;
 
+app.post("/todo", async function (req, res) {
+  const createPayload = req.body; // for post req
+  const parsePayload = createTodo.safeParse(createPayload); // brought zod for necessary steps
+  if (!parsePayload) {
+    //saying if payload fails send status 411
+    res.status(411).json({
+      msg: "you have sent a wrong data",
+    });
+    return;
+  }
+  //put it on mongodb;
+  // todo.find used to find all data in collection but create is used to create the data in database;
 
-app.post("/todo",function(req,res){
-    const createPayload = req.body; // for post req
-    const parsePayload = createTodo.safeParse(createPayload); // brought zod for necessary steps
-    if(!parsePayload){  //saying if payload fails send status 411
-        res.status(411).json({
-            msg:"you have sent a wrong data"
-        })
-        return;
-    }
-    //put it on mongodb;
+  await todo.create({
+    title: createPayload.title,
+    description: createPayload.title,
+  });
+  res.json({
+    msg: "todo created!",
+  });
+});
 
-})
+app.get("/todos", async function (req, res) {
+  const todos = await todo.find({});
+  res.json({
+    todos
+  })
+});
 
-app.get("/todos",function(req,res){
+app.put("/completed", function (req, res) {
+  const createPayload = req.body;
+  const parsePayload = updateTodo.safeParse(createPayload);
+  if (!parsePayload) {
+    res.status(411).json({
+      msg: "you have sent a wrong data",
+    });
+    return;
+  }
+});
 
-    res.json({
-        msg: "hello world!"
-    })
-})
-
-app.put("/completed",function(req,res){
-
-    const createPayload = req.body;
-    const parsePayload = updateTodo.safeParse(createPayload);
-    if(!parsePayload){
-        res.status(411).json({
-            msg:"you have sent a wrong data"
-        })
-        return;
-    }
-
-})
-
-app.listen(port,()=>{
-    console.log(`app listening on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
